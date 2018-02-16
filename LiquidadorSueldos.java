fd9f24b7abdbda83b83bdc40d8085c24e73d1aab
Federico <fedelanus09@gmail.com>
2018-02-16 12:22:19 -0300

    .

---
+++ LiquidadorSueldos.java
@@ -0,0 +1,29 @@
+public class LiquidadorSueldos {
+	private ArrayList<Empleado> empleados;
+	
+	ArrayList<String> empleados = new ArrayList<String>();
+	
+	public void altaEmpleado(Empleado empleado) {
+		int i;
+		for (i=0;i < 
+		empleados.add(Empleado);
+	}
+	
+	public void bajaEmpleado(Empleado empleado) {
+		empledos.remove(Empleado);
+	}
+	
+	public void modificarEmpleado(Empleado empleado) {
+		
+	}
+	
+	public Empleado buscarEmpleadoPorDni(int dni) {
+	
+	}
+	
+	public Empleado buscarEmpleadoPorNroLegajo(int nroLegajo) {
+	
+	}
+	
+	public void generarLiquidacionSueldos();
+}
